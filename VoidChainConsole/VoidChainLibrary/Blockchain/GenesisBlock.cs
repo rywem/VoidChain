01e0abf1a71d47de67a762b0db075023290a2e5d
cybervoid <4731255+cybervoid@users.noreply.github.com>
2017-10-19 11:12:17 -0400

    void chain, starting initialization sequence code base

--- VoidChainConsole/VoidChainLibrary/Blockchain/GenesisBlock.cs
+++ VoidChainConsole/VoidChainLibrary/Blockchain/GenesisBlock.cs
@@ -4,43 +4,7 @@ using VoidChainLibrary.Objects;
 namespace VoidChainLibrary.Blockchain
 {
 
-    public class VoidChain
-    {
-
-		byte[] hash1 = new byte[32];
-		byte[] hash2 = new byte[32];
-
-		string timestamp; //max length 255
-		string pubkey = string.Empty; //max length 132
-		//uint timestamp_len = 0;
-		//uint scriptSig_len = 0;
-		//uint pubkey_len = 0;
-		//uint pubkeyScript_len = 0;
-		uint nBits = 0;
-        Transaction transaction;
-        GenesisBlock Block;
-        public VoidChain(string publicKey, string timeStamp)
-        {
-            this.pubkey = publicKey;
-            this.timestamp = timeStamp;
-        }
-
-        public void Initialize()
-        {
-            if (pubkey.Length != 65)
-                throw new VoidChainException("Invalid public key");
-            if(timestamp.Length > 254 || timestamp.Length <= 0)
-                throw new VoidChainException("Invalid timestamp");
-            transaction.InitTransaction(Block.COIN);
-        }
-        public void Execute()
-        {
-            
-        }
-    }
-
-
-
+   
     public class GenesisBlock
     {
         public ulong COIN { get; } = 100000000;
@@ -61,49 +25,5 @@ namespace VoidChainLibrary.Blockchain
 
     }
 
-    public struct Transaction
-    {
-        /* Hash of TX */
-        public byte[][] merkleHash;// = new uint[32];
-        /* Tx serialization before hashing */
-        public byte[] serializedData; //uses ref, or * pointer in C
-        /* Tx Version */
-        public uint version;
-        /* Inputs */
-        public byte numInputs; //program assumes one input
-        public byte[] prevOutput;
-        public uint prevoutIndex;
-        public byte[] scriptSig; //uses ref, * pointer in C
-        public uint sequence;
-
-        /* Output */
-        public byte numOutputs; //program assumes one output
-        public ulong outValue;
-        public byte[] pubkeyScript;
-
-        /* Final */
-        public uint locktime; 
-
-        public void InitTransaction(ulong COIN)
-        {
-			// Set some initial data that will remain constant throughout the program
-			this.version = 1;
-			this.numInputs = 1;
-			this.numOutputs = 1;
-			this.locktime = 0;
-			this.prevoutIndex = 0xFFFFFFFF;
-			this.sequence = 0xFFFFFFFF;
-			this.outValue = 50 * COIN;
-
-            prevOutput = new byte[32];
-            //initialize a new byte array to zero, as there are no previous output values 
-            for (int i = 0; i < prevOutput.Length; i++)
-            {
-                prevOutput[i] = 0;
-            }
-
-            merkleHash = new byte[32][];
-
-        }
-    }
+  
 }
---
+++ VoidChainConsole/VoidChainLibrary/Blockchain/VoidChain.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using VoidChainLibrary.Objects;
+
+namespace VoidChainLibrary.Blockchain
+{
+	public class VoidChain
+	{
+
+		byte[] hash1 = new byte[32];
+		byte[] hash2 = new byte[32];
+
+		string timestamp; //max length 255
+		string pubkey = string.Empty; //max length 132
+		uint nBits = 0;
+        uint pubkey_len;
+        public GenesisBlock Block { get; private set; }
+		public Transaction transaction
+		{
+			get
+			{
+				return Block.transaction;
+			}
+		}
+
+		public VoidChain(string publicKey, string timeStamp)
+		{
+			this.pubkey = publicKey;
+			this.timestamp = timeStamp;
+            Initialize();
+		}
+
+		public void Initialize()
+		{
+			if (pubkey.Length != 65)
+				throw new VoidChainException("Invalid public key");
+			if (timestamp.Length > 254 || timestamp.Length <= 0)
+				throw new VoidChainException("Invalid timestamp");
+			//initialize the genesis block and set the initial transaction values
+			this.Block = new GenesisBlock(true);
+            pubkey_len = (uint)pubkey.Length >> 1;
+
+
+            // Encode pubkey to binary and prepend pubkey size, then append the OP_CHECKSIG byte
+            transaction.pubkeyScript = malloc((pubkey_len+2)*sizeof(uint8_t));
+
+		}
+		public void Execute()
+		{
+
+		}
+	}
+}
