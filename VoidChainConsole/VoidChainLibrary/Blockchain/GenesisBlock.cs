bcb150cd484c11b8e4bd847ad059fc35ede5fdb7
cybervoid <4731255+cybervoid@users.noreply.github.com>
2017-10-19 08:51:18 -0400

    Added extension methods to create hex values

---
+++ VoidChainConsole/VoidChainLibrary/Blockchain/GenesisBlock.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Numerics;
+using VoidChainLibrary.Objects;
+namespace VoidChainLibrary.Blockchain
+{
+    
+    public class GenesisBlock
+    {
+        public const ulong COIN = 100000000;
+		public const ulong CENT = 1000000;
+
+        public uint OP_CHECKSIG { get; set; } = 172; //expressed as 0xAC
+        public bool GenerateBlock { get; set; } = false;
+
+        public uint StartNonce { get; set; } = 0;
+        public uint UnixTime { get; set; } = 0;
+        public Transaction Tx; 
+
+        private void ByteSwap(int length, ref byte[] buf)
+        {
+            int i;
+            byte temp;
+
+            for (i = 0; i < length; i++)
+            {
+                temp = buf[i];
+                buf[i] = buf[length - i - 1];
+                buf[length - i - 1] = temp;
+            }
+        }
+
+    }
+
+    public struct Transaction
+    {
+        /* Hash of TX */
+        byte[] merkleHash;// = new uint[32];
+        /* Tx serialization before hashing */
+        byte[] serializedData; //uses ref, or * pointer in C
+        /* Tx Version */
+        uint version;
+        /* Inputs */
+        byte[] numInputs; //program assumes one input
+        byte[] prevOutput;
+        uint prevoutIndex;
+        byte[] scriptSig; //uses ref, * pointer in C
+        uint sequence;
+
+        /* Output */
+        byte[] numOuputs; //program assumes one output
+        ulong outValue;
+        byte[] pubkeyScript;
+
+        /* Final */
+        uint lockTime; 
+    }
+}
---
+++ VoidChainConsole/VoidChainLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VoidChainLibrary
+{
+    public class Class1
+    {
+    }
+}
