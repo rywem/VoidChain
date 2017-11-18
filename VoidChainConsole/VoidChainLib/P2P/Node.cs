dda5e8d9f93561b11261a95739c3d7073597c98a
cybervoid <4731255+cybervoid@users.noreply.github.com>
2017-11-18 07:05:28 -0500

    void chain node commits

---
+++ VoidChainConsole/VoidChainLib/P2P/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace VoidChainLib.P2P
+{
+    public class Node
+    {
+        public string HashId { get; set; }
+        public string PublicKey { get; set; }
+
+        public Node()
+        {
+        }
+
+        public void Initialize()
+        {
+            
+        }
+    }
+}
